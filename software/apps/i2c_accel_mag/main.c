913c7db7b936f7b7ca04cd2ee5f6997a74349160
ramawama <ramajanco24@gmail.com>
2026-02-24 00:55:51 -0600

    Software_apps

---
+++ software/apps/i2c_accel_mag/main.c
@@ -0,0 +1,45 @@
+// I2C accelerometer/magnetometer app
+//
+// Read from I2C accelerometer/magnetometer on the Microbit
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "nrf_delay.h"
+#include "nrf_twi_mngr.h"
+
+#include "microbit_v2.h"
+#include "lsm303agr.h"
+
+// Global variables
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 1, 0);
+
+int main(void) {
+  printf("Board started!\n");
+
+  // Initialize I2C peripheral and driver
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  // WARNING!!
+  // These are NOT the correct pins for external I2C communication.
+  // If you are using QWIIC or other external I2C devices, the are
+  // connected to EDGE_P19 (a.k.a. I2C_QWIIC_SCL) and EDGE_P20 (a.k.a. I2C_QWIIC_SDA)
+  i2c_config.scl = I2C_INTERNAL_SCL;
+  i2c_config.sda = I2C_INTERNAL_SDA;
+  i2c_config.frequency = NRF_DRV_TWI_FREQ_100K;
+  i2c_config.interrupt_priority = 0;
+  nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+
+  // Initialize the LSM303AGR accelerometer/magnetometer sensor
+  lsm303agr_init(&twi_mngr_instance);
+
+  //TODO: implement me!
+
+  // Loop forever
+  while (1) {
+    // Don't put any code in here. Instead put periodic code in a callback using a timer.
+    nrf_delay_ms(1000);
+  }
+}
+
